0342038e7c08f5f4413f7410a013e4e09a783cbf
doubaniux <goodsir@vivaldi.net>
2021-02-17 23:07:38 +0100

    add thumbnail feature

---
+++ common/templatetags/thumb.py
@@ -0,0 +1,15 @@
+from django import template
+from easy_thumbnails.templatetags.thumbnail import thumbnail_url
+
+register = template.Library()
+
+@register.filter
+def thumb(source, alias):
+    """
+    This filter modifies that from `easy_thumbnails` so that
+    it can neglect .svg file.
+    """
+    if source.url.endswith('.svg'):
+        return source.url
+    else:
+        return thumbnail_url(source, alias)
