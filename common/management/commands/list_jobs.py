4584fefc08e7746fa1192de01ca8ee6980c8f64b
Your Name <you@example.com>
2021-12-12 18:16:18 -0500

    add django command to manage rq jobs

---
+++ common/management/commands/list_jobs.py
@@ -0,0 +1,21 @@
+from django.core.management.base import BaseCommand
+import pprint
+from redis import Redis
+from rq.job import Job
+from rq import Queue
+
+
+class Command(BaseCommand):
+    help = 'Show jobs in queue'
+
+    def add_arguments(self, parser):
+        parser.add_argument('queue', type=str, help='Queue')
+
+    def handle(self, *args, **options):
+        redis = Redis()
+        queue = Queue(str(options['queue']), connection=redis)
+        for registry in [queue.started_job_registry, queue.deferred_job_registry, queue.finished_job_registry, queue.failed_job_registry, queue.scheduled_job_registry]:
+            self.stdout.write(self.style.SUCCESS(f'Registry {registry}'))
+            for job_id in registry.get_job_ids():
+                job = Job.fetch(job_id, connection=redis)
+                print(f'Job {job}')
