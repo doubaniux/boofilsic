8c90ff3a031874bf59c8d433f2285ba8e6647cd1
Your Name <you@example.com>
2021-12-21 00:09:37 -0500

    fix search 500

--- common/templatetags/highlight.py
+++ common/templatetags/highlight.py
@@ -10,8 +10,4 @@ register = template.Library()
 @register.filter
 @stringfilter
 def highlight(text, search):
-    to_be_replaced_words = set(re.findall(search, text, flags=re.IGNORECASE))
-
-    for word in to_be_replaced_words:
-        text = text.replace(word, f'<span class="highlight">{word}</span>')
-    return mark_safe(text)
+    return mark_safe(text.replace(search, f'<span class="highlight">{search}</span>'))  # TODO better query words match
