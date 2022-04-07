61a70d939761693e672b1eaa087c271aa0fec5ab
Your Name <you@example.com>
2022-04-06 23:55:15 -0400

    fix anonymous search

--- common/templatetags/neo.py
+++ common/templatetags/neo.py
@@ -6,4 +6,6 @@ register = template.Library()
 
 @register.simple_tag(takes_context=True)
 def current_user_marked_item(context, item):
-    return context['request'].user.get_mark_for_item(item)
+    if context['request'].user and context['request'].user.is_authenticated:
+        return context['request'].user.get_mark_for_item(item)
+    return None
