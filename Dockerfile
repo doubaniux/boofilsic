d38129ac9c077bc9eb56d320ca1e368c7d1b505c
doubaniux <goodsir@vivaldi.net>
2023-03-06 16:25:24 +0100

    containerize

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
-# syntax=docker/dockerfile:1
 FROM python:3.8-slim
-ENV PYTHONDONTWRITEBYTECODE=1
+
 ENV PYTHONUNBUFFERED=1
+
 RUN apt-get update \  
   && apt-get install -y --no-install-recommends build-essential libpq-dev git \  
   && rm -rf /var/lib/apt/lists/*
@@ -11,13 +11,8 @@ RUN pip install --no-cache-dir -r /tmp/requirements.txt \
     && useradd -U app_user \  
     && install -d -m 0755 -o app_user -g app_user /app/static
 
-ENV DJANGO_SETTINGS_MODULE=yoursettings.dev
 WORKDIR /app
 USER app_user:app_user
 COPY --chown=app_user:app_user . .
-RUN chmod +x docker/*.sh
 
-# Section 6- Docker Run Checks and Configurations 
-ENTRYPOINT [ "docker/entrypoint.sh" ]
-
-CMD [ "docker/start.sh", "server" ]
+ENTRYPOINT ["docker/start.sh"]
--- docker/start.sh
+++ docker/start.sh
@@ -1,36 +1,22 @@
 #!/bin/bash  
 
-cd /app  
+cd /app 
 
-if [ $# -eq 0 ]; then  
-    echo "Usage: start.sh <server|rq>"  
-    exit 1  
-fi  
+python manage.py collectstatic --noinput  
+python manage.py makemigrations users books movies games music sync mastodon management collection
+python manage.py makemigrations  
+python manage.py migrate users
+python manage.py migrate
+python manage.py rqworker --with-scheduler doufen export mastodon &
+# enqueue scheduled jobs per minute
+python manage.py rqscheduler --queue mastodon &
 
-PROCESS_TYPE=$1  
-
-if [ "$PROCESS_TYPE" = "server" ]; then  
-    if [ "$DJANGO_DEBUG" = "true" ]; then  
-        gunicorn \  
-            --reload \  
-            --bind 0.0.0.0:8000 \  
-            --workers 2 \  
-            --worker-class eventlet \  
-            --log-level DEBUG \  
-            --access-logfile "-" \  
-            --error-logfile "-" \  
-            boofilsic.wsgi 
-    else  
-        gunicorn \  
-            --bind 0.0.0.0:8000 \  
-            --workers 2 \  
-            --worker-class eventlet \  
-            --log-level DEBUG \  
-            --access-logfile "-" \  
-            --error-logfile "-" \  
-            boofilsic.wsgi
-    fi  
-elif [ "$PROCESS_TYPE" = "rq" ]; then
-    rqworker --with-scheduler doufen export mastodon
-fi
+gunicorn \
+    -b 0.0.0.0:8000 \
+    -w 2 \
+    -k uvicorn.workers.UvicornWorker \
+    --log-level DEBUG \
+    --access-logfile '-' \
+    --error-logfile '-' \
+    boofilsic.asgi:application
 
